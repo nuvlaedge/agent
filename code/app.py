8fd4eaba6e6a2b24ad9c20374432347e600d17c0
Cristovao Cordeiro <cris@sixsq.com>
2022-01-18 17:29:51 +0100

    wrap logging level setting within main

--- code/app.py
+++ code/app.py
@@ -26,29 +26,30 @@ import requests
 import time
 from flask import Flask, request, jsonify
 from threading import Event
-
-__copyright__ = "Copyright (C) 2019 SixSq"
-__email__ = "support@sixsq.com"
-
-parser = argparse.ArgumentParser(description="NuvlaBox Agent")
-parser.add_argument('--debug', dest='debug', default=False, action='store_true',
-                    help='use for increasing the verbosity level')
-
-if parser.parse_args().debug:
-    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-else:
-    try:
-        logging.basicConfig(stream=sys.stdout, level=logging.getLevelName(os.getenv('NUVLABOX_LOG_LEVEL')))
-    except ValueError:
-        logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-
-# finish imports with logging
-import agent.AgentApi as AgentApi
 from agent.common import NuvlaBoxCommon
 from agent.Activate import Activate
 from agent.Telemetry import Telemetry
 from agent.Infrastructure import Infrastructure
 from agent.Job import Job
+
+__copyright__ = "Copyright (C) 2019 SixSq"
+__email__ = "support@sixsq.com"
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="NuvlaBox Agent")
+    parser.add_argument('--debug', dest='debug', default=False, action='store_true',
+                        help='use for increasing the verbosity level')
+
+    if parser.parse_args().debug:
+        logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+    else:
+        try:
+            logging.basicConfig(stream=sys.stdout, level=logging.getLevelName(os.getenv('NUVLABOX_LOG_LEVEL')))
+        except ValueError:
+            logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+# finish imports with logging
+import agent.AgentApi as AgentApi
 
 app = Flask(__name__)
 data_volume = "/srv/nuvlabox/shared"
