3b7c39e1235b75d1a984399cd7a66f1b6e83bea1
schaubl <schaubl@users.noreply.github.com>
2022-11-16 22:38:18 +0100

    Port of: Default gw report (#118)
    Cherry-pick of commit: bb818be2a6a8754a9f190068f5149183913559a2

--- code/agent/monitor/data/network_data.py
+++ code/agent/monitor/data/network_data.py
@@ -4,11 +4,21 @@
 
 Gathers all the requirements for status reporting
 """
-from typing import Union, Dict
+from typing import Union, Dict, List, Optional
 
 from pydantic import Field
 
 from agent.monitor import BaseDataStructure
+
+
+class IP(BaseDataStructure, allow_mutation=False):
+    """
+    address: IP addresses
+    """
+    address: str = ''
+
+    def __hash__(self):
+        return hash(self.address)
 
 
 class NetworkInterface(BaseDataStructure):
@@ -16,29 +26,38 @@ class NetworkInterface(BaseDataStructure):
     Pydantic BaseModel definition for Network interfaces. This includes public,
     vpn, and swarm addresses.
         iface_name: network interface name
-        ip: IPv4 address
+        ips: List of IP addresses
         default_gw: flag indicating whether the interface is the default for the host
         device or not
     """
 
     iface_name: Union[str, None] = Field(alias='interface')
-    ip: Union[str, None]
+    ips: List[IP] = Field([])
     default_gw: bool = Field(False, alias='default-gw')
 
     # Interface data traffic control
-    tx_bytes: Union[int, None] = Field(alias='bytes-transmitted')
-    rx_bytes: Union[int, None] = Field(alias='bytes-received')
+    tx_bytes: Union[int, None] = Field(0, alias='bytes-transmitted')
+    rx_bytes: Union[int, None] = Field(0, alias='bytes-received')
+
+
+class IPAddresses(BaseDataStructure):
+    """
+    public: Public IPv4 and IPv6 addresses
+    local: Host device local interface and its corresponding IP addresses
+    vpn: VPN IPv4 address provided by OpenVpn server (If present)
+    swarm: SWARM node IP address (If present)
+    """
+    public: str = ''
+    swarm: str = ''
+    vpn: str = ''
+    local: str = ''
 
 
 class NetworkingData(BaseDataStructure):
     """
     Base model to gather all the IP addresses in the NuvlaEdge device
-        public: Public IPv4 and IPv6 addresses
-        local: Host device local interfaces and its corresponding IP addresses
-        vpn: VPN IPv4 address provided by OpenVpn server (If present)
-        swarm: SWARM node IP address (If present)
+
     """
-    public: NetworkInterface = NetworkInterface(iface_name="public")
-    local: Union[Dict[str, NetworkInterface], None] = {}
-    vpn: Union[NetworkInterface, None]
-    swarm: Union[NetworkInterface, None]
+    default_gw: Optional[str]
+    interfaces: Dict[str, NetworkInterface] = {}
+    ips: IPAddresses = IPAddresses()
