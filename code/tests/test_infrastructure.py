55ac4fbd80a24232ec937dfb64adba454670f73d
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2023-04-14 15:23:59 +0200

    fix hotspot error: use non routable ip in tests

--- code/tests/test_infrastructure.py
+++ code/tests/test_infrastructure.py
@@ -399,17 +399,20 @@ class InfrastructureTestCase(unittest.TestCase):
                             mock_get_comp_endpoint, mock_attr_changed,
                             mock_swarm_token_diff, mock_get_capabilities, mock_needs_partial_decommission,
                             mock_do_commission, mock_write_file, mock_get_tls_keys):
+
+        swarm_endpoint = 'https://127.1.1.1:5000'
+
         self.obj.container_runtime.get_join_tokens.return_value = ()
         mock_needs_cluster_commission.return_value = {'cluster-managers': ['node-id']}
         mock_read_commission.return_value = {}
         self.obj.telemetry_instance.get_vpn_ip.return_value = '1.1.1.1'
-        mock_get_comp_endpoint.return_value = ('https://1.1.1.1:5000', 5000)
+        mock_get_comp_endpoint.return_value = (swarm_endpoint, 5000)
         self.obj.container_runtime.get_node_labels.return_value = None
         mock_attr_changed.return_value = None
 
         mock_get_tls_keys.return_value = ('ca', 'cert', 'key')
         self.obj.container_runtime.define_nuvla_infra_service.return_value = {
-            'swarm-endpoint': 'https://1.1.1.1:5000',
+            'swarm-endpoint': swarm_endpoint,
             'swarm-client-ca': 'ca',
             'swarm-client-cert': 'cert',
             'swarm-client-key': 'key'
@@ -429,7 +432,7 @@ class InfrastructureTestCase(unittest.TestCase):
         self.obj.try_commission()
 
         self.obj.container_runtime.define_nuvla_infra_service.assert_called_with(
-            'https://1.1.1.1:5000',
+            swarm_endpoint,
             ('ca', 'cert', 'key'))
         # and if there are no joining tokens, then commissioning_attr_has_changed is only called 2
         self.assertEqual(1, mock_attr_changed.call_count,
@@ -443,12 +446,12 @@ class InfrastructureTestCase(unittest.TestCase):
         self.obj.try_commission()
 
         self.obj.container_runtime.define_nuvla_infra_service.assert_called_with(
-            'https://1.1.1.1:5000',
+            swarm_endpoint,
             ('ca', 'cert', 'key'))
 
         # given the aforementioned return_values, we expect the following commissioning payload to be sent and saved
         expected_payload = {
-            'swarm-endpoint': 'https://1.1.1.1:5000',
+            'swarm-endpoint': swarm_endpoint,
             'swarm-client-ca': 'ca',
             'swarm-client-cert': 'cert',
             'swarm-client-key': 'key',
