f4ca3f2b104be893bc339c9523ed0c95d932e71e
giovannibianco <john.white@sixsq.com>
2023-05-09 13:28:44 +0200

    Changes in order to get the get_coe_client

--- code/agent/orchestrator/factory.py
+++ code/agent/orchestrator/factory.py
@@ -1,20 +1,31 @@
-"""
-Orchestration factory
-"""
-
 import os
 
-from agent.orchestrator import ContainerRuntimeClient
 from agent.orchestrator.docker import DockerClient
 from agent.orchestrator.kubernetes import KubernetesClient
 
+if os.getenv('KUBERNETES_SERVICE_HOST'):
+    ORCHESTRATOR = KubernetesClient.NAME
+    ORCHESTRATOR_COE = KubernetesClient.NAME_COE
+else:
+    ORCHESTRATOR = DockerClient.NAME
+    ORCHESTRATOR_COE = DockerClient.NAME_COE
 
-def get_container_runtime() -> ContainerRuntimeClient:
+
+HOSTSFS = "/rootfs"
+
+
+def orchestrator_name():
+    return ORCHESTRATOR
+
+
+def get_coe_client(installation_home, hostfs=HOSTSFS):
     """
-    Instantiate the right container runtime client based on the underlying COE
+    Returns COE client based on the underlying orchestrator.
+
     :return: instance of a ContainerRuntimeClient
     """
-    if os.getenv('KUBERNETES_SERVICE_HOST'):
-        return KubernetesClient()
-    else:
-        return DockerClient()
+    if orchestrator_name() == KubernetesClient.NAME:
+        return KubernetesClient(hostfs, installation_home)
+    if orchestrator_name() == DockerClient.NAME:
+        return DockerClient(hostfs, installation_home)
+    raise NotImplementedError(f'COE client of type {ORCHESTRATOR} is not known.')
